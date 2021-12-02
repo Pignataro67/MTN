5731aefb7dbd309806e870cbed983298db088935
Pignataro67 <Pignataro67@gmail.com>
2021-12-02 04:44:38 -0800

    JS

---
+++ script.js
@@ -0,0 +1,8 @@
+function hideAllContents() {
+  contents.forEach(content => content.classList.remove('show'))
+}
+
+
+function hideAllItems() {
+  listItems.forEach(item => item.classList.remove('active'))
+}
